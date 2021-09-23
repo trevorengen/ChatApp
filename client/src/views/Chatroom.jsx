b826ea150ffa789f28a21018697a4506f1fe3306
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-09-22 18:37:52 -0700

    new build

--- client/src/views/Chatroom.jsx
+++ client/src/views/Chatroom.jsx
@@ -25,7 +25,7 @@ const Chatroom = (props) => {
         color: theme.palette.text.secondary,
     }));
 
-    const [socket] = useState(() => io('http://3.129.195.240:8000/', { transports: WebSocket }));
+    const [socket] = useState(() => io('http://3.129.195.240:8000/'));
     const [message, setMessage] = useState('');
     const [allMessages, setAllMessages] = useState([]);
     const [open, setOpen] = useState(false);
