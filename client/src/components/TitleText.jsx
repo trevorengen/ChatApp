d1241aa8ccaee49d758a3db83338e05e8ea6331c
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-10-26 14:52:05 -0700

    No animation cause it was bugged.

--- client/src/components/TitleText.jsx
+++ client/src/components/TitleText.jsx
@@ -1,15 +1,11 @@
 import { Typography } from '@mui/material';
 import React, { useEffect } from 'react'
-import Typical from 'react-typical'
 
 const TitleText = () => {
 
     return (
         <Typography variant='h1' style={{height: '150px', margin: '50px 0'}}>
-            <Typical 
-                steps={['Hi', 500, 'Welcome to viddy chat', 3000]}
-                wrapper='p'
-            />
+            Hi! Welcome to viddychat!
         </Typography>);
 };
 
